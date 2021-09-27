cfec92be7f3809f3b3ae6be2ec4bb4fd8a9b8a8c
Artem Shahoval <artem10-messi10@ukr.net>
2021-09-27 12:00:37 +0200

    added Portal for Modal component

--- user-form/src/components/modal/modal.js
+++ user-form/src/components/modal/modal.js
@@ -1,16 +1,26 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import Button from "../UI/button/button";
 import Card from "../UI/card/card";
 
+const MyModal = (props) => {
+  return (
+    <Card>
+      <h5>{props.tittle}</h5>
+      <p>{props.message}</p>
+      <Button onClick={props.hideModal}>Okay</Button>
+    </Card>
+  );
+};
+
 const Modal = ({ hideModal, tittle, message }) => {
   return (
-    <>
-      <Card>
-        <h5>{tittle}</h5>
-        <p>{message}</p>
-        <Button onClick={hideModal}>Okay</Button>
-      </Card>
-    </>
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <MyModal hideModal={hideModal} tittle={tittle} message={message} />,
+        document.getElementById("modal-root")
+      )}
+    </React.Fragment>
   );
 };
 
