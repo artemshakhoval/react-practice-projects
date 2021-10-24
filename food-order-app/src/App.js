4da2998ef9da2c21f2c7ebae52f957d9c694a50c
Artem Shahoval <artem10-messi10@ukr.net>
2021-10-24 20:35:21 +0300

    add new food order app project

---
+++ food-order-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/layout/header";
+import MealsSummary from "./components/meals/mealsSummary";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <MealsSummary />
+    </>
+  );
+};
+
+export default App;
