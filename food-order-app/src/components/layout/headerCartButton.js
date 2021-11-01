52ca99b142375f0a6a571ca75880266d00d960f4
Artem Shahoval <artem10-messi10@ukr.net>
2021-11-01 10:42:59 +0200

    add animation to cart button

--- food-order-app/src/components/layout/headerCartButton.js
+++ food-order-app/src/components/layout/headerCartButton.js
@@ -1,18 +1,42 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../cart/cartIcon";
 
 import classes from "./headerCartButton.module.css";
 
 const HeaderCartButton = ({ showModalHandler }) => {
+  const [btnIsHighlighted, setBntIsHighlighted] = useState(false);
+
   const ctx = useContext(CartContext);
 
-  //const numberOfItems = ctx.items.length;
-  const numberOfItems = ctx.items.reduce((currNumber, item) => {
-    return currNumber + item.amount;
-  }, 0);
+  const { items } = ctx;
+
+  const numberOfItems = items.length;
+  // const numberOfItems = items.reduce((currNumber, item) => {
+  //   return currNumber + item.amount;
+  // }, 0);
+
+  const bntClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBntIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBntIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={showModalHandler}>
+    <button className={bntClasses} onClick={showModalHandler}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
