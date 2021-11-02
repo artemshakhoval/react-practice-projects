176618f98487dc9d0cf5dd0c1a54ffec866d8adb
Artem Shahoval <artem10-messi10@ukr.net>
2021-11-02 16:16:39 +0200

    add store component

---
+++ redux-project/src/store/index.js
@@ -0,0 +1,23 @@
+import { createStore } from "redux";
+
+const initState = {
+  counter: 0,
+};
+
+const counterReducer = (state = initState, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "derement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  return state;
+};
+
+const store = createStore(counterReducer);
+
+export default store;
