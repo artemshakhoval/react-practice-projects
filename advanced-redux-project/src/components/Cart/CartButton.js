95526fab8328584188ee15cc94de79fd617cfddd
Artem Shahoval <artem10-messi10@ukr.net>
2021-11-05 22:59:09 +0200

    refactoring code with adding full add/remove items state

--- advanced-redux-project/src/components/Cart/CartButton.js
+++ advanced-redux-project/src/components/Cart/CartButton.js
@@ -1,8 +1,9 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { cartActions } from "../../store/ui-slice";
 import classes from "./CartButton.module.css";
 
 const CartButton = (props) => {
+  const cartQuantity = useSelector((state) => state.cart.totalQuantity);
   const dispatch = useDispatch();
 
   const showCartHandler = () => {
@@ -11,7 +12,7 @@ const CartButton = (props) => {
   return (
     <button className={classes.button} onClick={showCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
--- advanced-redux-project/src/store/cart-slice.js
+++ advanced-redux-project/src/store/cart-slice.js
@@ -10,19 +10,32 @@ const cartSlice = createSlice({
   initialState: cartInitState,
   reducers: {
     addItems(state, action) {
-      const newItem = action.payload; //// have item
+      const newItem = action.payload; //// gave item
       const existingItem = state.items.find((item) => item.id === newItem.id); //// find item in array
       state.totalQuantity++; /// when add new item to cart change cart's quantity
       if (!existingItem) {
+        /// if new item is different from another we push it
         state.items.push({
-          id: newItem.id,
+          itemId: newItem.id,
+          price: newItem.price,
           totalPrice: newItem.totalPrice,
-          totalAmount: newItem.totalAmount,
+          quantity: 1,
+          title: newItem.title,
         });
       } else {
+        existingItem.quantity++; /// if new item is same then change quantity of existing item
+        existingItem.totalPrice = existingItem.totalPrice + existingItem.price; /// add new item's price to totalPrice
       }
     },
-    removeItems() {},
+    removeItems(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      if (existingItem === 1) {
+        state.items = state.items.filter((item) => item.id !== id); /// if we have only 1 item and want to delete it then we remove this item from our items array
+      } else {
+        existingItem.quantity--; /// if we have 2 or more items then just decrement quantity of item
+      }
+    },
   },
 });
 
