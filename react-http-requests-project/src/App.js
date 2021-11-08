8a80758dd3a5668a51c980a86ed4c9a3c43387c2
Artem Shahoval <artem10-messi10@ukr.net>
2021-11-08 23:30:21 +0200

    make post method and send movie to backend and get it back using http get method

--- react-http-requests-project/src/App.js
+++ react-http-requests-project/src/App.js
@@ -13,25 +13,51 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch(
+        "https://react-http-b087d-default-rtdb.firebaseio.com/movies.json"
+      );
       if (!response.ok) {
         throw new Error("Something went wrong!");
       }
 
       const data = await response.json();
+      console.log(data);
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      let loadedMovies = [];
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
+    // try {
+    //   const response = await fetch("https://swapi.dev/api/films/");
+    //   if (!response.ok) {
+    //     throw new Error("Something went wrong!");
+    //   }
+
+    //   const data = await response.json();
+
+    //   const transformedMovies = data.results.map((movieData) => {
+    //     return {
+    //       id: movieData.episode_id,
+    //       title: movieData.title,
+    //       openingText: movieData.opening_crawl,
+    //       releaseDate: movieData.release_date,
+    //     };
+    //   });
+    //   setMovies(transformedMovies);
+    // } catch (error) {
+    //   setError(error.message);
+    // }
     setIsLoading(false);
   }, []);
 
