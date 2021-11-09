9da65efce2351b5f4a5dbda0f7da9dac1a782b0c
Artem Shahoval <artem10-messi10@ukr.net>
2021-11-09 22:57:37 +0200

    add event handlers to input

--- react-forms-inputs/src/components/SimpleInput.js
+++ react-forms-inputs/src/components/SimpleInput.js
@@ -1,9 +1,34 @@
+import React, { useState } from "react";
+
 const SimpleInput = (props) => {
+  const [inputNameValue, setInputNameValue] = useState("");
+  // const inputNameRef = useRef();
+
+  const inputNameChangeHandler = (e) => {
+    setInputNameValue(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    // const nameRefValue = inputNameRef.current.value;
+    // console.log(nameRefValue);
+    console.log(inputNameValue);
+
+    setInputNameValue("");
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={submitHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          // ref={inputNameRef}
+          type="text"
+          id="name"
+          onChange={inputNameChangeHandler}
+          value={inputNameValue}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
