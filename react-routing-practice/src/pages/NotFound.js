075dfc20b231e6fd63e03d1713de23a5abc53c9d
Artem Shahoval <artem10-messi10@ukr.net>
2021-11-19 00:07:04 +0200

    adding Not Found page if url isn't correct

---
+++ react-routing-practice/src/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../index.css";
+
+const NotFound = () => {
+  return (
+    <div className="centered">
+      <p>Not Found!</p>
+    </div>
+  );
+};
+
+export default NotFound;
