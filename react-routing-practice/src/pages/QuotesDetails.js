34e5e8702acff28c22adad075b457ce4187e43a1
Artem Shahoval <artem10-messi10@ukr.net>
2021-11-18 23:59:13 +0200

    find quote id to show details using params

--- react-routing-practice/src/pages/QuotesDetails.js
+++ react-routing-practice/src/pages/QuotesDetails.js
@@ -2,14 +2,21 @@ import React from "react";
 import { useParams } from "react-router";
 import { Route } from "react-router-dom";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "Learning React is fun!" },
+  { id: "q2", author: "Tom", text: "Learning React is great!" },
+];
 
 const QuotesDetails = () => {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quotesId);
+
   return (
     <>
-      <div>Quotes Details</div>
-      <p>{params.quotesId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quotesId}/comments`}>
         <Comments />
       </Route>
